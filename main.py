9e6fe8984d987828b8bb54a950df9deb773fb5f1
tuantran0910 <tntuan0910@gmail.com>
2026-05-05 22:14:07 +0700

    chore: initialize project

---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from testdbtgithubactions!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ projects/rainbow_dbt_duckdb/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from rainbow-dbt-duckdb!")
+
+
+if __name__ == "__main__":
+    main()
